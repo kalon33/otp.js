f5614e7b182026c28f48f8f99c0c5cce81a679ac
Marko Burjek <s0mebody.slo@gmail.com>
2015-07-06 09:15:18 +0200

    Moved availible languages object to config

--- config.js
+++ config.js
@@ -8,5 +8,9 @@ window.OTP_config = {
   otpApi: 'http://otp-test.nysdot.conveyal.com/otp/routers/',
   esriApi: 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/',
 
-  reverseGeocode: true
+  reverseGeocode: true,
+
+  //Here should be all languages that we have translations for
+  //Order doesn't matter
+  availible_languages: {"de":true, "fr":true, "it":true, "sl":true, "en":true}
 };
