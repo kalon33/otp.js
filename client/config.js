702c005c150b54eefdc324103ab304603e1833ac
Marko Burjek <s0mebody.slo@gmail.com>
2015-07-07 16:59:29 +0200

    Added preliminary support for language switcher

--- client/config.js
+++ client/config.js
@@ -11,7 +11,152 @@ window.OTP_config = {
 
   //Here should be all languages that we have translations for
   //Order doesn't matter
-  availible_languages: {"de":true, "fr":true, "it":true, "sl":true, "en":true, "ca_ES":true},
+  availible_languages: {
+      "de": {
+        //Name of a language written in a language itself (Used in Frontend to
+        //choose a language)
+        name: 'Deutsch',
+        //FALSE-imperial units are used
+        //TRUE-Metric units are used
+        metric : true, 
+        //Name of localization file (*.po file) in src/client/i18n
+        locale_short : "de",
+        //Name of datepicker localization in
+        //src/client/js/lib/jquery-ui/i18n (usually
+        //same as locale_short)
+        //this is index in $.datepicker.regional array
+        //If file for your language doesn't exist download it from here
+        //https://github.com/jquery/jquery-ui/tree/1-9-stable/ui/i18n
+        //into src/client/js/lib/jquery-ui/i18n
+        //and add it in index.html after other localizations
+        //It will be used automatically when UI is switched to this locale
+        datepicker_locale_short: "de",
+        //Currently unused:
+        format         : "D. MM. YYYY H:mm", //momentjs
+        date_format    : "DD.MM.YYYY", //momentjs
+        time_format    : "H:mm", //prej je blo H:i momentjs
+    },
+    "fr" : {
+        //Name of a language written in a language itself (Used in Frontend to
+        //choose a language)
+        name: 'Français',
+        //FALSE-imperial units are used
+        //TRUE-Metric units are used
+        metric : true, 
+        //Name of localization file (*.po file) in src/client/i18n
+        locale_short : "fr",
+        //Name of datepicker localization in
+        //src/client/js/lib/jquery-ui/i18n (usually
+        //same as locale_short)
+        //this is index in $.datepicker.regional array
+        //If file for your language doesn't exist download it from here
+        //https://github.com/jquery/jquery-ui/tree/1-9-stable/ui/i18n
+        //into src/client/js/lib/jquery-ui/i18n
+        //and add it in index.html after other localizations
+        //It will be used automatically when UI is switched to this locale
+        datepicker_locale_short: "fr",
+        //currently unused
+        format         : "DD.MM.YY [à] H:mm", //moment.js
+        date_format    : "DD/MM/YYYY", //momentjs 
+        time_format    : "HH:mm", //momentjs
+    },
+    "it": {
+        //Name of a language written in a language itself (Used in Frontend to
+        //choose a language)
+        name: 'Italiano',
+        //FALSE-imperial units are used
+        //TRUE-Metric units are used
+        metric : true,
+        //Name of localization file (*.po file) in src/client/i18n
+        locale_short : "it",
+        //Name of datepicker localization in
+        //src/client/js/lib/jquery-ui/i18n (usually
+        //same as locale_short)
+        //this is index in $.datepicker.regional array
+        //If file for your language doesn't exist download it from here
+        //https://github.com/jquery/jquery-ui/tree/1-9-stable/ui/i18n
+        //into src/client/js/lib/jquery-ui/i18n
+        //and add it in index.html after other localizations
+        //It will be used automatically when UI is switched to this locale
+        datepicker_locale_short: "it",
+        //Currently unused
+        format: "DD.MM.YYYY, HH:mm", //momentjs
+        date_format: "DD/MM/YYYY", //momentjs
+        time_format: "HH:mm", //momentjs
+    },
+    "sl": {
+        //Name of a language written in a language itself (Used in Frontend to
+        //choose a language)
+        name: 'Slovensko',
+        //FALSE-imperial units are used
+        //TRUE-Metric units are used
+        metric : true, 
+        //Name of localization file (*.po file) in src/client/i18n
+        locale_short : "sl",
+        //Name of datepicker localization in
+        //src/client/js/lib/jquery-ui/i18n (usually
+        //same as locale_short)
+        //this is index in $.datepicker.regional array
+        //If file for your language doesn't exist download it from here
+        //https://github.com/jquery/jquery-ui/tree/1-9-stable/ui/i18n
+        //into src/client/js/lib/jquery-ui/i18n
+        //and add it in index.html after other localizations
+        //It will be used automatically when UI is switched to this locale
+        datepicker_locale_short: "sl",
+        //Currently unused
+        format         : "D. MM. YYYY H:mm", //momentjs
+        date_format    : "DD.MM.YYYY", //momentjs
+        time_format    : "H:mm", //momentjs
+    },
+    "ca_ES": {
+        //Name of a language written in a language itself (Used in Frontend to
+        //choose a language)
+        name: 'Català',
+        //FALSE-imperial units are used
+        //TRUE-Metric units are used
+        metric : true, 
+        //Name of localization file (*.po file) in src/client/i18n
+        locale_short : "ca_ES",
+        //Name of datepicker localization in
+        //src/client/js/lib/jquery-ui/i18n (usually
+        //same as locale_short)
+        //this is index in $.datepicker.regional array
+        //If file for your language doesn't exist download it from here
+        //https://github.com/jquery/jquery-ui/tree/1-9-stable/ui/i18n
+        //into src/client/js/lib/jquery-ui/i18n
+        //and add it in index.html after other localizations
+        //It will be used automatically when UI is switched to this locale
+        datepicker_locale_short: "ca",
+        //Currently unused
+        format         : "DD M HH:mm", //momentjs
+        date_format    : "DD/MM/YYYY", //momentjs must be same as date_picker format which is by default: mm/dd/yy
+        time_format    : "HH:mm", //momentjs
+    },
+    "en": {
+        //Name of a language written in a language itself (Used in Frontend to
+        //choose a language)
+        name: 'English',
+        //FALSE-imperial units are used
+        //TRUE-Metric units are used
+        metric : false, 
+        //Name of localization file (*.po file) in src/client/i18n
+        locale_short : "en",
+        //Name of datepicker localization in
+        //src/client/js/lib/jquery-ui/i18n (usually
+        //same as locale_short)
+        //this is index in $.datepicker.regional array
+        //If file for your language doesn't exist download it from here
+        //https://github.com/jquery/jquery-ui/tree/1-9-stable/ui/i18n
+        //into src/client/js/lib/jquery-ui/i18n
+        //and add it in index.html after other localizations
+        //It will be used automatically when UI is switched to this locale
+        datepicker_locale_short: "", //Doesn't use localization
+        //Currently unused
+        format         : "MMM Do YYYY, h:mma", //moment.js
+        date_format    : "MM/DD/YYYY", //momentjs must be same as date_picker format which is by default: mm/dd/yy
+        time_format    : "h:mma", //momentjs
+    },
+  },
 
   cookieName: "wanted_language",
 
