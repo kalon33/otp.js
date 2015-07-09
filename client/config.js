b03991c153a8729b90981bee19b37b0b47284f75
Marko Burjek <s0mebody.slo@gmail.com>
2015-07-09 16:15:32 +0200

    Date and time uses user specified format
     not pure momentjs local aware format. This was made because all local
    aware moment.js time and date formats have year in them which is too
    long and looks bad in narrative display. So we specified our own
    formats for each language which are the same as lll moment.js formats
    only without the year.

--- client/config.js
+++ client/config.js
@@ -31,8 +31,7 @@ window.OTP_config = {
         //and add it in index.html after other localizations
         //It will be used automatically when UI is switched to this locale
         datepicker_locale_short: "de",
-        //Currently unused:
-        format         : "D. MM. YYYY H:mm", //momentjs
+        format         : "D. MMM LT", //momentjs
         date_format    : "DD.MM.YYYY", //momentjs
         time_format    : "H:mm", //prej je blo H:i momentjs
     },
@@ -55,8 +54,7 @@ window.OTP_config = {
         //and add it in index.html after other localizations
         //It will be used automatically when UI is switched to this locale
         datepicker_locale_short: "fr",
-        //currently unused
-        format         : "DD.MM.YY [à] H:mm", //moment.js
+        format         : "D MMM LT", //moment.js
         date_format    : "DD/MM/YYYY", //momentjs 
         time_format    : "HH:mm", //momentjs
     },
@@ -79,8 +77,7 @@ window.OTP_config = {
         //and add it in index.html after other localizations
         //It will be used automatically when UI is switched to this locale
         datepicker_locale_short: "it",
-        //Currently unused
-        format: "DD.MM.YYYY, HH:mm", //momentjs
+        format: "D MMM LT", //momentjs
         date_format: "DD/MM/YYYY", //momentjs
         time_format: "HH:mm", //momentjs
     },
@@ -103,8 +100,7 @@ window.OTP_config = {
         //and add it in index.html after other localizations
         //It will be used automatically when UI is switched to this locale
         datepicker_locale_short: "sl",
-        //Currently unused
-        format         : "D. MM. YYYY H:mm", //momentjs
+        format         : "D. MMM LT", //momentjs
         date_format    : "DD.MM.YYYY", //momentjs
         time_format    : "H:mm", //momentjs
     },
@@ -127,8 +123,7 @@ window.OTP_config = {
         //and add it in index.html after other localizations
         //It will be used automatically when UI is switched to this locale
         datepicker_locale_short: "ca",
-        //Currently unused
-        format         : "DD M HH:mm", //momentjs
+        format         : "D MMM LT", //momentjs
         date_format    : "DD/MM/YYYY", //momentjs must be same as date_picker format which is by default: mm/dd/yy
         time_format    : "HH:mm", //momentjs
     },
@@ -151,8 +146,7 @@ window.OTP_config = {
         //and add it in index.html after other localizations
         //It will be used automatically when UI is switched to this locale
         datepicker_locale_short: "", //Doesn't use localization
-        //Currently unused
-        format         : "MMM Do YYYY, h:mma", //moment.js
+        format         : "MMM Do h:mma", //moment.js
         date_format    : "MM/DD/YYYY", //momentjs must be same as date_picker format which is by default: mm/dd/yy
         time_format    : "h:mma", //momentjs
     },
